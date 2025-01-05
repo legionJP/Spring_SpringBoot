f885e74bdcbf8fa125a9a96cdc57ee10f8c020db
legionjp <jjpsingh18@gmail.com>
2025-01-06 00:12:17 +0530

    Adding autowire for config based

---
+++ Springjp/src/main/java/com/jspring6/AlienInterfaceConfig.java
@@ -0,0 +1,46 @@
+package com.jspring6;
+
+public class AlienInterfaceConfig {
+    
+private int age;
+private Computer com;
+private Laptop lap;
+
+// constructor
+public AlienInterfaceConfig() {
+    System.out.println("AlienSpring obj created");
+}
+
+public void coding() {
+    System.out.println("Alien Interface coding Now ");
+    com.compile();  
+    lap.compile();
+}
+
+public int getAge() {
+    return age;
+}
+
+public void setAge(int age) {
+    this.age = age;
+}
+
+public Computer getCom() {
+    return com;
+}
+
+public void setCom(Computer com) {
+    this.com = com;
+}
+
+public Laptop getLap() {
+    return lap;
+}
+
+public void setLap(Laptop lap) {
+    this.lap = lap;
+}
+
+
+
+}
