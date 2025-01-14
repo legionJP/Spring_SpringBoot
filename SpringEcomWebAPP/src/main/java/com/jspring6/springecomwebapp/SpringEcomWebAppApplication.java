8e572fbaddfb90cea7b8a36213f567073ab11403
legionjp <jjpsingh18@gmail.com>
2025-01-15 00:49:38 +0530

    Adding the SpringEcomWebApp

---
+++ SpringEcomWebAPP/src/main/java/com/jspring6/springecomwebapp/SpringEcomWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.jspring6.springecomwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcomWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEcomWebAppApplication.class, args);
+    }
+
+}
---
+++ SpringEcomWebAPP/src/test/java/com/jspring6/springecomwebapp/SpringEcomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspring6.springecomwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcomWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
