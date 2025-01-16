bbd4469c4c0d91a4ce3471b3f92ce23dea3133a7
legionjp <jjpsingh18@gmail.com>
2025-01-16 06:00:21 +0530

    Addin gthe Before Aspect

--- SpringBootRestAOP/src/main/java/com/jspring6/springbootrestaop/aop/LoggingAspect.java
+++ SpringBootRestAOP/src/main/java/com/jspring6/springbootrestaop/aop/LoggingAspect.java
@@ -1,16 +1,24 @@
 package com.jspring6.springbootrestaop.aop;
-
-
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
-
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@Aspect  // so this is part of aspect 
 public class LoggingAspect {
     
     // if any  method is called maintain the logs 
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
+// Before  Returning 
+
+// return type first , then class name full path, args  
+    // @Before("execution(* *.*(..))")   // all the return type , all the class * and .* all the methods , (..) args 
+    @Before("execution(* com.jspring6.springbootrestaop.service.JobService.*(..))")
     public void logMethodCall()
     {
         LOGGER.info("Method Called");
